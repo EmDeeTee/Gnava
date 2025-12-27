a78b1a10c3aaeadfe9d018429fc2c76f72ea6449
Michał <m12@onet.com.pl>
2025-12-27 13:13:39 +0100

    Add functionality to create settlement menu item

--- src/main/java/Gnava/Interface/Menu/Actions/CreateSettlementAction.java
+++ src/main/java/Gnava/Interface/Menu/Actions/CreateSettlementAction.java
@@ -3,6 +3,7 @@ package Gnava.Interface.Menu.Actions;
 import Gnava.Game.GameState;
 import Gnava.Game.Settlements.Settlement;
 import Gnava.Game.Settlements.SettlementPopulationType;
+import Gnava.Interface.Popups.Presets.CreateSettlementPopup;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,8 +12,6 @@ public class CreateSettlementAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Create settlement action");
-        GameState.getInstance().tryCreateSettlement(
-            new Settlement("Hello", 10, 100, SettlementPopulationType.DWARF)
-        );
+        GameState.getInstance().tryCreateSettlement(new CreateSettlementPopup().show());
     }
 }
