523727f6134361502b005c7019b12c875863caf8
Michał <m12@onet.com.pl>
2025-12-28 12:25:41 +0100

    Add TODO and throw on error in initial settlement creation

--- src/main/java/Gnava/Main.java
+++ src/main/java/Gnava/Main.java
@@ -6,8 +6,14 @@ import Gnava.Interface.Popups.Presets.CreateSettlementPopup;
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: Maybe introduce GameFrame::onGameStart()?
+
         GameFrame gameFrame = GameFrame.getInstance();
         gameFrame.setVisible(true);
-        new CreateSettlementPopup(true).show().ifPresent(s -> GameState.getInstance().tryCreateSettlement(s));
+
+        new CreateSettlementPopup(true).show().ifPresentOrElse(
+                s -> GameState.getInstance().tryCreateSettlement(s),
+                () -> { throw new IllegalStateException("Settlement creation cancelled"); }
+        );
     }
 }
