b35f42219fd8e4f0bc7a2205194c53e78896f83c
Michał <m12@onet.com.pl>
2025-12-29 23:21:14 +0100

    Use EnumMap instead of ArrayList for translations

--- src/main/java/Gnava/Interface/Translations/TranslationTableCrustyDutch.java
+++ src/main/java/Gnava/Interface/Translations/TranslationTableCrustyDutch.java
@@ -1,13 +1,21 @@
 package Gnava.Interface.Translations;
 
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class TranslationTableCrustyDutch implements TranslationTable {
-    private final Map<TranslationKey, String> table = Map.ofEntries(
-        Map.entry(TranslationKey.GKINGDOMS, "Koninkrijken van Gnava"),
-        Map.entry(TranslationKey.CREATE_SETTLEMENT, "Creëren Schikking"),
-        Map.entry(TranslationKey.WELCOME_MESSAGE, "Welkom, naar de Koninkrijken van Gnava. Jij zijn A godachtig wezen toezicht houden dit wereld. <br><br> Creëren jouw schikking en hulp het worden de meest krachtig en voorspoedig land in de rijk.")
-    );
+    private final Map<TranslationKey, String> table;
+
+    public TranslationTableCrustyDutch() {
+        EnumMap<TranslationKey, String> map = new EnumMap<>(TranslationKey.class);
+
+        map.put(TranslationKey.GKINGDOMS, "Koninkrijken van Gnava");
+        map.put(TranslationKey.CREATE_SETTLEMENT, "Creëren Schikking");
+        map.put(TranslationKey.WELCOME_MESSAGE, "Welkom, naar de Koninkrijken van Gnava. Jij zijn A godachtig wezen toezicht houden dit wereld. <br><br> Creëren jouw schikking en hulp het worden de meest krachtig en voorspoedig land in de rijk.");
+
+        table = Collections.unmodifiableMap(map);
+    }
 
     @Override
     public String language() {
--- src/main/java/Gnava/Interface/Translations/TranslationTableEnglish.java
+++ src/main/java/Gnava/Interface/Translations/TranslationTableEnglish.java
@@ -1,13 +1,21 @@
 package Gnava.Interface.Translations;
 
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class TranslationTableEnglish implements TranslationTable {
-    private final Map<TranslationKey, String> table = Map.ofEntries(
-        Map.entry(TranslationKey.GKINGDOMS, "Kingdoms of Gnava"),
-        Map.entry(TranslationKey.CREATE_SETTLEMENT, "Create Settlement"),
-        Map.entry(TranslationKey.WELCOME_MESSAGE, "Welcome, to the Kingdoms of Gnava. You are a god-like being overseeing this world. <br><br> Create your settlement and help it become the most powerful and prosperous land in the realm.")
-    );
+    private final Map<TranslationKey, String> table;
+
+    public TranslationTableEnglish() {
+        EnumMap<TranslationKey, String> map = new EnumMap<>(TranslationKey.class);
+
+        map.put(TranslationKey.GKINGDOMS, "Kingdoms of Gnava");
+        map.put(TranslationKey.CREATE_SETTLEMENT, "Create Settlement");
+        map.put(TranslationKey.WELCOME_MESSAGE, "Welcome, to the Kingdoms of Gnava. You are a god-like being overseeing this world. <br><br> Create your settlement and help it become the most powerful and prosperous land in the realm.");
+
+        table = Collections.unmodifiableMap(map);
+    }
 
     @Override
     public String language() {
