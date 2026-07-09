4f28603e76acbd8ec25ba24ba863334db7ff5428
Michał <m12@onet.com.pl>
2026-07-09 16:32:35 +0200

    Add NOTE

--- Gnava.Core/src/main/java/Gnava/Core/GameState.java
+++ Gnava.Core/src/main/java/Gnava/Core/GameState.java
@@ -3,6 +3,8 @@ package Gnava.Core;
 import org.springframework.stereotype.Component;
 
 // NOTE: Actually, why does this class even exist?
+// Maybe turn this class into a GameClock of sorts?
+// So it will own the current time, and then mutate it in the TimeManager?
 @Component
 public class GameState {
     private int currentDay = 0;
