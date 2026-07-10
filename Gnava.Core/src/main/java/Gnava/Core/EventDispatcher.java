4d8037141fc55010df472729dc9e1da0f00f5580
Michał <m12@onet.com.pl>
2026-07-11 00:43:15 +0200

    Deprecate EventDispatcher

--- Gnava.Core/src/main/java/Gnava/Core/EventDispatcher.java
+++ Gnava.Core/src/main/java/Gnava/Core/EventDispatcher.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+@Deprecated
 public class EventDispatcher<T> {
     private final List<Consumer<T>> listeners = new ArrayList<>();
 
