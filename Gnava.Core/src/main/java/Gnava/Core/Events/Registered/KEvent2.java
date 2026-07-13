1335eb02d0172b5c8e3d2f811d77aeb93b66ab35
Michał <m12@onet.com.pl>
2026-07-14 00:06:37 +0200

    Implement KEvent2

---
+++ Gnava.Core/src/main/java/Gnava/Core/Events/Registered/KEvent2.java
@@ -0,0 +1,54 @@
+package Gnava.Core.Events.Registered;
+
+import Gnava.Core.Events.AbstractGameEventDefinition;
+import Gnava.Core.Events.Conditions.EventCondition;
+import Gnava.Core.Events.Conditions.Settlement.MinimumSettlementPopulationCondition;
+import Gnava.Core.Events.Contexts.SettlementEventContext;
+import Gnava.Core.Events.IGameEventDefinition;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public final class KEvent2 extends AbstractGameEventDefinition<SettlementEventContext> {
+    @Override
+    protected List<EventCondition<SettlementEventContext>> conditions() {
+        return List.of(
+            new MinimumSettlementPopulationCondition(100)
+        );
+    }
+
+    @Override
+    protected String resolveDescription(SettlementEventContext context) {
+        return "There is talk of a very dangerous person roaming the realm at night.<br><br>It got so bad that 100 %s escaped from %s."
+            .formatted(
+                context.getRandomTargetSettlement().orElseThrow().getPopulationType().plural(),
+                context.getRandomTargetSettlement().orElseThrow().getName()
+            );
+    }
+
+    @Override
+    protected String resolveTitle(SettlementEventContext context) {
+        return "Rumors of ruin";
+    }
+
+    @Override
+    public boolean isStoryEvent() {
+        return true;
+    }
+
+    @Override
+    public boolean firesOnce() {
+        return true;
+    }
+
+    @Override
+    public float probability() {
+        return 0.04f;
+    }
+
+    @Override
+    public List<Class<? extends IGameEventDefinition<?>>> prerequisites() {
+        return List.of(KEvent.class);
+    }
+}
--- Gnava.Core/src/main/java/Gnava/Core/Settlements/Enums/SettlementPopulationType.java
+++ Gnava.Core/src/main/java/Gnava/Core/Settlements/Enums/SettlementPopulationType.java
@@ -3,5 +3,13 @@ package Gnava.Core.Settlements.Enums;
 public enum SettlementPopulationType {
     GNOME,
     DWARF,
-    GOBLIN,
+    GOBLIN;
+
+    public String plural() {
+        return switch (this) {
+            case GNOME -> "gnomes";
+            case DWARF -> "dwarves";
+            case GOBLIN -> "goblins";
+        };
+    }
 }
