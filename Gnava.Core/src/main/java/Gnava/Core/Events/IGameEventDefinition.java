97d902be4d13582cd0c45b137db8e688b1775a88
Michał <m12@onet.com.pl>
2026-07-17 15:30:06 +0200

    Update TODO

--- Gnava.Core/src/main/java/Gnava/Core/Events/IGameEventDefinition.java
+++ Gnava.Core/src/main/java/Gnava/Core/Events/IGameEventDefinition.java
@@ -5,6 +5,7 @@ import Gnava.Core.Events.Contexts.EventContext;
 import java.util.List;
 
 // TODO: I think more methods from AbstractGameEvent belong here
+// Also, maybe this interface is redundant to begin with
 public interface IGameEventDefinition<C extends EventContext> {
     ExecutedGameEvent happen(C context);
 
