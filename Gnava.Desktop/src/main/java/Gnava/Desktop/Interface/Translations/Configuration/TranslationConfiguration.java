bfe3affc7b4cfbb0a122d76ba9868df2bb7f32c4
Michał <m12@onet.com.pl>
2026-07-19 11:32:27 +0200

    Add debug log to locale selection

--- Gnava.Desktop/src/main/java/Gnava/Desktop/Interface/Translations/Configuration/TranslationConfiguration.java
+++ Gnava.Desktop/src/main/java/Gnava/Desktop/Interface/Translations/Configuration/TranslationConfiguration.java
@@ -1,5 +1,7 @@
 package Gnava.Desktop.Interface.Translations.Configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,14 +9,22 @@ import java.util.Locale;
 
 @Configuration
 public class TranslationConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TranslationConfiguration.class);
+
     @Bean
     public Locale locale() {
-        if ("1".equals(System.getenv("USE_CRUSTY_DUTCH"))) {
+        boolean forceUseCrustyDutch = "1".equals(System.getenv("USE_CRUSTY_DUTCH"));
+        if (forceUseCrustyDutch) {
+            LOGGER.info("USE_CRUSTY_DUTCH is set to true. Forcing crusty dutch");
             return Locale.of("crustyDutch");
         }
 
-        return System.getProperty("os.name").startsWith("Windows")
+        LOGGER.debug("Guessing the locale...");
+        Locale selectedLocale = System.getProperty("os.name").startsWith("Windows")
             ? Locale.ENGLISH
             : Locale.of("crustyDutch");
+
+        LOGGER.debug("Selected {} as locale", selectedLocale);
+        return selectedLocale;
     }
 }
