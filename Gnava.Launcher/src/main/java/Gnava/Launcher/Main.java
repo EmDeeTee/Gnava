e123d50256343ca16916acf47cd3d248641a74b2
Michał <m12@onet.com.pl>
2026-07-19 02:06:56 +0200

    Add logging

--- Gnava.Launcher/src/main/java/Gnava/Launcher/Main.java
+++ Gnava.Launcher/src/main/java/Gnava/Launcher/Main.java
@@ -1,14 +1,19 @@
 package Gnava.Launcher;
 
 import Gnava.Desktop.Gnava;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(GnavaConfiguration.class);
         Gnava gnava = context.getBean(Gnava.class);
 
+        LOGGER.info("Gnava starting UI");
         gnava.initUi();
     }
 }
